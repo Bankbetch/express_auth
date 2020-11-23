537d72a73b1c3b68f4f83dc0658e3d37269b30cf
bank <jiraphun.jrb@gmail.com>
2020-11-24 00:37:02 +0700

    config middleware roles&permissions

--- routes/api/role.js
+++ routes/api/role.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const controllers = require('../../controllers/role.controller')
-
-router.get('/', controllers.onGetAll)
-router.get('/:id', controllers.onGetById)
-router.post('/', controllers.onInsert)
-router.put('/:id', controllers.onUpdate)
-router.delete('/:id', controllers.onDelete)
+const getDataToken = require('../rolePermissions')
+// ['menuName', 'USERS_VIEW']
+router.get('/', getDataToken.getTokenFromHeader(['ROLES', 'ROLES_VIEW']), controllers.onGetAll)
+router.get('/:id', getDataToken.getTokenFromHeader(['ROLES', 'ROLES_VIEW']), controllers.onGetById)
+router.post('/', getDataToken.getTokenFromHeader(['ROLES', 'ROLES_CREATE']), controllers.onInsert)
+router.put('/:id', getDataToken.getTokenFromHeader(['ROLES', 'ROLES_EDIT']), controllers.onUpdate)
+router.delete('/:id', getDataToken.getTokenFromHeader(['ROLES', 'ROLES_DELETE']), controllers.onDelete)
 
 module.exports = router
